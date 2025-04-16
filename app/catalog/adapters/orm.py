53e09be18656f3291c472bde2d1d4adaad5dd455
Sergey Listov <slistov@it-aces.com>
2025-04-16 14:09:08 +0000

    categories: foreign key added

--- app/catalog/adapters/orm.py
+++ app/catalog/adapters/orm.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, Integer, MetaData, String, Table
+from sqlalchemy import Column, ForeignKey, Integer, MetaData, String, Table
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import registry, relationship
 
+from app.catalog.domain import model
 from app.config import DATABASE_URL
 
 engine = create_async_engine(
@@ -16,7 +18,18 @@ categories = Table(
     meta,
     Column("id", Integer, primary_key=True),
     Column("name", String(255)),
-    Column("parent_id", Integer),
+    Column("parent_id", Integer, ForeignKey("categories.id")),
+)
+
+mapper_registry = registry()
+mapper_registry.map_imperatively(
+    model.Item,
+    categories,
+    properties={
+        "categories": relationship(
+            model.Item, backref="categories", order_by=categories.c.id
+        )
+    },
 )
 
 
